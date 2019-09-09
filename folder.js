f5a76b4813f91bffa1b58a364189e0ee65e379c4
Roma <exaple@mail.com>
2019-09-09 21:29:10 +0300

    modification folder.js

--- folder.js
+++ folder.js
@@ -1,1 +1,13 @@
-"use strict"
+'use strict'
+
+let persone = {
+    name: "Anzor",
+    age: 22,
+    isMarried: false, 
+    role: lolka
+};
+
+console. log(persone["name"]);
+
+alert("Anzor Privet");
+
