c03bb4ee3c94758c275b459ac878ad997285bb75
Roma <exaple@mail.com>
2019-09-14 17:24:44 +0300

    Sanya lolka

---
+++ Functions.js
@@ -0,0 +1,17 @@
+// let num = 20;
+
+// function Massege(text){
+//     alert(text);
+//     console.log(num); 
+// }
+
+// Massege("Hello World!");
+// console.log(num);
+
+function calc(a,b) {
+    return (a + b);
+}
+
+console.log(calc(3,4));
+
+console.log(calc(8,4));
